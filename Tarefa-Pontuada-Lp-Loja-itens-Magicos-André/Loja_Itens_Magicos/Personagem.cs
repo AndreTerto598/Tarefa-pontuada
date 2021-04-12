009ae028ae30e0a32d3b4b309b38eebf425d1f25
AndreTerto598 <82418709+AndreTerto598@users.noreply.github.com>
2021-04-12 19:16:13 -0300

    Add files via upload

---
+++ Tarefa-Pontuada-Lp-Loja-itens-Magicos-André/Loja_Itens_Magicos/Personagem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Loja_Itens_Magicos
+{
+    public class Personagem
+    {
+        public string Nome;
+        public Int32 Ouro;
+        public List<Item> Inventario = new List<Item>();
+
+        public void ApresentacaoAventureiro(Personagem aventureiro)
+        {
+            Console.Clear();
+           Console.WriteLine("Olá,meu nome é: ");
+           aventureiro.Nome = Console.ReadLine();
+           Console.WriteLine("Vendedor: Quantas moedas de ouro você possui,"+aventureiro.Nome +"?");
+           aventureiro.Ouro = int.Parse(Console.ReadLine());
+        }
+
+        public String Comprar()
+        {
+            
+            Console.Write("Eu quero comprar: ");
+            string carrinho = Console.ReadLine();
+            return carrinho;
+        }
+
+        public void ClienteEscolha(Loja loja, Personagem aventureiro)
+        {
+            Console.Clear();
+            int alternativa;
+            
+            do
+            {
+                Console.WriteLine("Máquina:Digite 1 para: - Acessar o catálogo da loja.");
+                Console.WriteLine("Máquina:Digite 2 para: -  Comprar itens."); 
+                Console.WriteLine("Máquina:Digite 3 para: - Acessar Inventário");
+                Console.WriteLine("0 - voltar");
+                alternativa = Convert.ToInt16(Console.ReadLine());
+                if (alternativa == 1)
+                {
+                    loja.ApresentarCatalogo();
+                }
+                else if (alternativa == 2 && loja.VerificarEstoque() == true)
+                {
+                    Console.Clear();
+                    loja.ApresentarCatalogo();
+                    loja.Vender(aventureiro.Comprar(), aventureiro);
+                }else if(alternativa == 3)
+                {
+                    Console.Clear();
+                    if(aventureiro.Inventario.Count == 0)
+                    {
+                        Console.WriteLine("\nMinhas moedas: " + aventureiro.Ouro + "\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nMinhas moedas: " + aventureiro.Ouro);
+                        foreach (var inv in aventureiro.Inventario)
+                        {
+                            Console.WriteLine("\nNome: " + inv.Nome);
+                            Console.WriteLine("Descrição: " + inv.Desc);
+                            Console.WriteLine("Quantidade: " + inv.Quant + "\n");
+                        }
+                    }
+                }
+                else if (loja.VerificarEstoque() == false)
+                {
+                    Console.Clear();
+                    Console.WriteLine("\nEstoque Vazio,volte mais tarde");
+                }
+                else if (alternativa != 0)
+                {
+                    Console.WriteLine("Entrada Invalida");
+                }
+
+            } while (alternativa != 0);
+            Console.Clear();
+        }
+    }
+}
---
+++ Tarefa-Pontuada-Lp-Loja-itens-Magicos-André/Loja_Itens_Magicos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Loja_Itens_Magicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int alternativa = 0;
+            Loja loja = new Loja();
+            Personagem aventureiro = new Personagem();
+
+            do
+            {
+                Console.WriteLine("Máquina:Digite 1 para: - Menu:Vendedor");
+                Console.WriteLine("Máquina:Digite 2 para: - Menu:Cliente");
+                Console.WriteLine("Máquina:Digite 0 para: - encerrar \n");
+                alternativa = Convert.ToInt16(Console.ReadLine());
+
+                if (alternativa == 1)
+                {
+                    loja.AddCatalogo();
+
+                }
+                else if (alternativa == 2)
+                {
+                    aventureiro.ApresentacaoAventureiro(aventureiro);
+                    aventureiro.ClienteEscolha(loja, aventureiro);
+                }
+                else if (alternativa != 0)
+                {
+                    Console.WriteLine("Entrada Invalida");
+                }
+            } while (alternativa != 0);
+            
+        }
+
+        
+
+    }
+}
